64e8172b3524cb7d26de98c16a72576447f96a3a
= <alanivon8@gmail.com>
2016-05-07 21:41:43 +0800

    add rspec and basic test for model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+	it 'is accessible' do
+		product = Product.create!
+		expect(product).to eq(Product.last)
+	end	
+
+	it 'has right columns' do
+		columns = Product.column_names
+		expect(columns).to include("id")
+		expect(columns).to include("price")
+		expect(columns).to include("description")
+		expect(columns).to include("quantity")
+	end
+end
