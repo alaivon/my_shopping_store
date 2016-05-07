ccce338d2c6df3f0582b11e8ace02bbc05783f84
= <alanivon8@gmail.com>
2016-05-07 23:21:27 +0800

    add admin required function for backgstage

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,28 +1,22 @@
 class Admin::ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :admin_required
 
-  # GET /products
-  # GET /products.json
   def index
     @products = Product.all
   end
 
-  # GET /products/1
-  # GET /products/1.json
   def show
   end
 
-  # GET /products/new
   def new
     @product = Product.new
   end
 
-  # GET /products/1/edit
   def edit
   end
 
-  # POST /products
-  # POST /products.json
   def create
     @product = Product.new(product_params)
       if @product.save
@@ -33,8 +27,6 @@ class Admin::ProductsController < ApplicationController
 
   end
 
-  # PATCH/PUT /products/1
-  # PATCH/PUT /products/1.json
   def update
     @product = Product.update(product_params)
 		if @product.save
@@ -44,20 +36,23 @@ class Admin::ProductsController < ApplicationController
     end		
   end
 
-  # DELETE /products/1
-  # DELETE /products/1.json
   def destroy
    @product.destroy
    redirect_to admin_products_path
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
+
+  def admin_required
+    if !current_user.admin?
+      redirect_to '/'
+    end
+  end
+
   def set_product
     @product = Product.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def product_params
     params.require(:product).permit(:title, :description, :quantity, :price)
   end
