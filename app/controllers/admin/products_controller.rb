4eb53668ca48a501a53051efee18ebe450ea9418
= <alanivon8@gmail.com>
2016-05-07 22:11:53 +0800

    add CRUD code

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,65 @@
 class Admin::ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  # GET /products
+  # GET /products.json
+  def index
+    @products = Product.all
+  end
+
+  # GET /products/1
+  # GET /products/1.json
+  def show
+  end
+
+  # GET /products/new
+  def new
+    @product = Product.new
+  end
+
+  # GET /products/1/edit
+  def edit
+  end
+
+  # POST /products
+  # POST /products.json
+  def create
+    @product = Product.new(product_params)
+      if @product.save
+        redirect_to admin_product_url(@product), notice: 'Product was successfully created.'
+      else
+			  render :new 
+      end
+
+  end
+
+  # PATCH/PUT /products/1
+  # PATCH/PUT /products/1.json
+  def update
+    @product = Product.update(product_params)
+		if @product.save
+    	redirect_to admin_product_url(@product), notice: 'Product was successfully created.'
+    else
+			render :new 
+    end		
+  end
+
+  # DELETE /products/1
+  # DELETE /products/1.json
+  def destroy
+   @product.destroy
+   redirect_to admin_products_path
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def product_params
+    params.require(:product).permit(:title, :description, :quantity, :price)
+  end
+
 end
