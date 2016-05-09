0594f753c46b1189a5b0fc6b732330ce711418b4
= <alanivon8@gmail.com>
2016-05-09 18:53:26 +0800

    test user_controller_spec

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe Admin::UsersController, type: :controller do
   	get :index
   	expect(response).to have_http_status(200)
   end
+
+  it '#to_normal' do
+  	user = create(:user)
+  	user.to_normal
+  	post :to_normal, id: user.id
+  	expect(user.is_admin).to eq(false)
+  	expect(response).to have_http_status(302)
+  	expect(response).to redirect_to(admin_users_url)
+  end
+
+  it '#to_admin' do
+  	user1 = User.create(email: "test1@test.com", password: 1234567, password_confirmation: 1234567, is_admin: false)
+  	user1.to_admin
+  	post :to_admin, id: user1.id
+  	expect(user1.is_admin).to eq(true)
+  	expect(response).to have_http_status(302)
+  	expect(response).to redirect_to(admin_users_url)
+  end
 end
