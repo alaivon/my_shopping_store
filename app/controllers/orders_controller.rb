da56f95f74d664b3c43939feaf02b3457d7dbe5c
= <alanivon8@gmail.com>
2016-05-10 16:41:26 +0800

    basic checkout function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,13 +4,35 @@ class OrdersController < ApplicationController
 	before_action :set_cart, only: [:create, :new]
 
 	def new
-		
+		if @current_cart.cart_items.empty?
+			flash[:warning] = "Your cart is empty!"
+			redirect_to products_url
+		else
+			@order = current_user.orders.build
+			@info = @order.build_info
+		end
 	end
+
+	def create
+		@order = current_user.orders.build(order_params)
+		if @order.save
+			@order.add_items_from_cart(@current_cart)
+			@order.calculate_total!(@current_cart)
+			redirect_to root_url
+		else
+			render :new
+		end
+	end
+
+
 
 	private
 
-	def test_quantity
-		
+	def order_params
+		params.require(:order).permit(info_attributes: [:billing_name, :billing_address, 
+																										:shipping_name, :shipping_address])
 	end
+
+
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,22 @@
 class Order < ActiveRecord::Base
 	belongs_to :user
-
 	has_many :items, class_name: 'OrderItem', dependent: :destroy
-	has_many :info, class_name: 'OrderInfo', dependent: :destroy
+	has_one :info, class_name: 'OrderInfo', dependent: :destroy
+
+	accepts_nested_attributes_for :info
+
+	def add_items_from_cart(cart)
+		cart.cart_items.each do |cart_item|
+			item = items.build
+			item.name = cart_item.product.title
+			item.quantity = cart_item.quantity
+			item.price = cart_item.price
+			item.save
+		end
+	end
+
+	def calculate_total!(cart)
+		self.total = cart.total_price
+		self.save
+	end
 end
