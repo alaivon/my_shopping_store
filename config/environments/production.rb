25dfea253beb105c9972ceed115ec23e440ad6d6
= <alanivon8@gmail.com>
2016-05-11 18:03:56 +0800

    setting production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-
+   config.action_mailer.default_url_options = { host: 'fierce-eyrie-44051.herokuapp.com' }
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
   config.action_mailer.delivery_method = :smtp
